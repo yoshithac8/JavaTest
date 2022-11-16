4fd8480fe1fe8a26d8f08a940f56190b1887f85e
NJ24-LAB <81625019+NJ24-LAB@users.noreply.github.com>
2022-11-16 18:00:39 +0530

    Update and rename deploy.sh to dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 230601024369.dkr.ecr.ap-south-1.amazonaws.com
 docker build -t javatest .
 docker tag javatest:latest 230601024369.dkr.ecr.ap-south-1.amazonaws.com/javatest:latest
