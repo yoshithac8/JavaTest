1e3a300a2928ff241556fa99a75abacf73ca4578
NJ24-LAB <81625019+NJ24-LAB@users.noreply.github.com>
2022-11-17 17:19:29 +0530

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 230601024369.dkr.ecr.ap-south-1.amazonaws.com
-docker build -t javatest .
-docker tag javatest:latest 230601024369.dkr.ecr.ap-south-1.amazonaws.com/javatest:latest
-docker push 230601024369.dkr.ecr.ap-south-1.amazonaws.com/javatest:latest
+docker build -t demo-${pomversion} .
+docker tag demo-${pomversion} 230601024369.dkr.ecr.ap-south-1.amazonaws.com/demo-${pomversion}
+docker push 230601024369.dkr.ecr.ap-south-1.amazonaws.com/demo-${pomversion}
