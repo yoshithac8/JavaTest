8f0ad96aa3985e6885131bfaf272d51416fce709
NJ24-LAB <81625019+NJ24-LAB@users.noreply.github.com>
2022-11-17 17:44:13 +0530

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dockertag=${dockertag:?"Missing Tag Name"}
-DOCKER_TAG=${dockertag}
+pomversion=${pomversion:?"Missing Tag Name"}
+DOCKER_TAG=${pomversion}
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 230601024369.dkr.ecr.ap-south-1.amazonaws.com
 docker build -t demo-${DOCKER_TAG} .
 docker tag demo-${DOCKER_TAG} 230601024369.dkr.ecr.ap-south-1.amazonaws.com/demo-${DOCKER_TAG}
