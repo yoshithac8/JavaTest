4447fab34852ef92597d45175bed2da503ca122d
NJ24-LAB <81625019+NJ24-LAB@users.noreply.github.com>
2022-11-18 08:38:57 +0530

    Update and rename dockerbuild.sh to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ aws ecr get-login-password --region ap-south-1 | docker login --username AWS --p
 docker build -t demo:${DOCKER_TAG} .
 docker tag demo:${DOCKER_TAG} 230601024369.dkr.ecr.ap-south-1.amazonaws.com/demo:${DOCKER_TAG}
 docker push 230601024369.dkr.ecr.ap-south-1.amazonaws.com/demo:${DOCKER_TAG}
+helm upgrade -f ./deploy/JavaTest/values.yaml --kube-context=testcluster --namespace default --install javatest --set image.repository=230601024369.dkr.ecr.ap-south-1.amazonaws.com/demo,image.tag=${DOCKER_TAG},timestamp=`date +t%s` ./deploy/JavaTest --debug --wait
+
